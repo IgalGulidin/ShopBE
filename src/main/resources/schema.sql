97df64c90b795545f68c2cbb13ef979794bbe475
unknown <gulidinigal@gmail.com>
2026-02-04 17:23:21 +0200

    schema build

---
+++ src/main/resources/schema.sql
@@ -0,0 +1,53 @@
+DROP TABLE IF EXISTS order_items;
+DROP TABLE IF EXISTS favorites;
+DROP TABLE IF EXISTS orders;
+DROP TABLE IF EXISTS items;
+DROP TABLE IF EXISTS users;
+
+CREATE TABLE users (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    first_name VARCHAR(60) NOT NULL,
+    last_name VARCHAR(60) NOT NULL,
+    email VARCHAR (120) NOT NULL,
+    password_hash VARCHAR(255) NOT NULL,
+    phone VARCHAR (30),
+    country VARCHAR(60) NOT NULL,
+    city VARCHAR(60) NOT NULL
+);
+
+CREATE TABLE items (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(120) NOT NULL,
+    image_url VARCHAR(400) NOT NULL,
+    price_usd DECIMAL(10,2) NOT NULL,
+    stock_qty INT NOT NULL
+);
+
+CREATE TABLE favorites (
+    user_id INT NOT NULL,
+    item_id INT NOT NULL,
+    PRIMARY KEY (user_id, item_id),
+    CONSTRAINT fk_fav_user FOREIGN KEY (user_id) REFERENCES users(id),
+    CONSTRAINT fk_fav_item FOREIGN KEY (item_id) REFERENCES items(id),
+);
+
+CREATE TABLE orders (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    user_id INT NOT NULL,
+    created_at TIMESTAMP NOT NULL,
+    ship_country VARCHAR(60) NOT NULL,
+    ship_city VARCHAR(60) NOT NULL,
+    total_price DECIMAL(10,2) NOT NULL,
+    status VARCHAR(10) NOT NULL,
+    CONSTRAINT fk_order_user FOREIGN KEY (user_id) REFERENCES users(id)
+);
+
+CREATE TABLE order_items (
+    order_id INT NOT NULL,
+    item_id INT NOT NULL,
+    quantity INT NOT NULL,
+    unit_price DECIMAL(10,2) NOT NULL,
+    PRIMARY KEY (order_id, item_id),
+    CONSTRAINT fk_oi_order FOREIGN KEY (order_id) REFERENCES orders(id),
+    CONSTRAINT fk_oi_item FOREIGN KEY (item_id) REFERENCES items(id)
+);
