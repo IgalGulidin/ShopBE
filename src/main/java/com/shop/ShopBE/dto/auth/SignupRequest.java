1525f0bd41c674f2297eaae777c0bf780c447741
unknown <gulidinigal@gmail.com>
2026-02-06 16:00:58 +0200

    added Signup DTO context

--- src/main/java/com/shop/ShopBE/dto/auth/SignupRequest.java
+++ src/main/java/com/shop/ShopBE/dto/auth/SignupRequest.java
@@ -1,4 +1,14 @@
 package com.shop.ShopBE.dto.auth;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+
 public class SignupRequest {
+    @NotBlank public String firstName;
+    @NotBlank public String lastName;
+    @Email @NotBlank public String email;
+    @NotBlank public String password;
+    public String phone;
+    @NotBlank public String country;
+    @NotBlank public String city;
 }
