c1fdc9cf0069a4d6cab2db571996ac8eb724bcb6
unknown <gulidinigal@gmail.com>
2026-02-06 17:29:26 +0200

    ItemController build

--- src/main/java/com/shop/ShopBE/controller/ItemController.java
+++ src/main/java/com/shop/ShopBE/controller/ItemController.java
@@ -1,4 +1,34 @@
 package com.shop.ShopBE.controller;
 
+
+import com.shop.ShopBE.dto.item.ItemResponse;
+import com.shop.ShopBE.service.ItemService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/items")
 public class ItemController {
+
+    private final ItemService itemService;
+
+    public ItemController(ItemService itemService) {
+        this.itemService = itemService;
+    }
+
+    @GetMapping
+    public List<ItemResponse> getAll() {
+        return itemService.getAll();
+    }
+
+    @GetMapping
+    public List<ItemResponse> search(@RequestParam String query) {
+        return itemService.search(query);
+    }
+
+    @GetMapping("/{id}")
+    public ItemResponse getById(@PathVariable long id) {
+        return itemService.getById(id);
+    }
 }
