a3108b0da8471e1d9051a37f58c3ce2535bde64d
unknown <gulidinigal@gmail.com>
2026-02-16 15:36:45 +0200

    FavoriteController test + added missing @Configuration in WebConfig

--- src/main/java/com/shop/ShopBE/config/WebConfig.java
+++ src/main/java/com/shop/ShopBE/config/WebConfig.java
@@ -1,8 +1,11 @@
 package com.shop.ShopBE.config;
 
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+
+@Configuration
 public class WebConfig implements WebMvcConfigurer {
 
     private final AuthInterceptor authInterceptor;
--- src/main/java/com/shop/ShopBE/controller/FavoriteController.java
+++ src/main/java/com/shop/ShopBE/controller/FavoriteController.java
@@ -1,4 +1,19 @@
 package com.shop.ShopBE.controller;
 
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/favorites")
 public class FavoriteController {
+
+    @GetMapping
+    public List<Long> getFavorites(@RequestAttribute("userId") long userId) {
+        return List.of();
+    }
 }
