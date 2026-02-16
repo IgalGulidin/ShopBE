a955321d9abd3ebb150dae6cd45be22a19716077
unknown <gulidinigal@gmail.com>
2026-02-16 14:21:10 +0200

    WebConfig build

--- src/main/java/com/shop/ShopBE/config/WebConfig.java
+++ src/main/java/com/shop/ShopBE/config/WebConfig.java
@@ -1,4 +1,23 @@
 package com.shop.ShopBE.config;
 
-public class WebConfig {
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+public class WebConfig implements WebMvcConfigurer {
+
+    private final AuthInterceptor authInterceptor;
+
+    public WebConfig(AuthInterceptor authInterceptor) {
+        this.authInterceptor = authInterceptor;
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(authInterceptor)
+                .addPathPatterns(
+                        "/api/favorites/**",
+                        "/api/orders/**",
+                        "/api/users/**"
+                );
+    }
 }
